d7d09d39dab3330146d1e864d7c27de222c59e3a
Daniël Illouz <daniel.illouz@me.com>
2018-03-20 12:52:22 +0100

    Create hello world v2 app

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# gitops-hello-world-v2
+# GitOps Hello World v2
 Second iteration of the Hello world REST API used to demonstrate a more decoupled GitOps workflow.
+
+## Infrastructure Config
+
+* [Staging](https://github.com/crowdynews/gitops-hello-world-infra-staging-v2)
+
+## Running locally
+
+```
+npm i && npm start
+```
+
+The following env vars can be set:
+
+| PROPERTY   | REQUIRED | DEFAULT VALUE                   |
+| ---------- | -------- | ------------------------------- |
+| `API_HOST` | no       | `0.0.0.0` set in `./src/app.js` |
+| `API_PORT` | no       | `8888` set in `./src/app.js`    |
---
+++ src/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+
+const { API_PORT = 8888, API_HOST = '0.0.0.0' } = process.env;
+
+const app = express();
+
+app.get('/', function helloWorld(req, res) {
+  res.jsonp({
+    text: 'Hello World GitOps v2!'
+  });
+});
+
+app.use(function notFound(req, res) {
+  res.status(404).jsonp({
+    error: 'Not Found'
+  });
+});
+
+// prettier-ignore
+app.use(function handleErrors(err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.status(500).jsonp({
+    error: err.message || 'Internal Server Error'
+  });
+});
+
+app.listen(API_PORT, API_HOST, () => {
+  console.log(`The server is running on http://${API_HOST}:${API_PORT}/`);
+});
